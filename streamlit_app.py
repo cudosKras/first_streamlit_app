57ed2dee64e708c8c9f8b0ac2f4f488fcf876fe0
Serhii Krasnoiarskiy <Serhii.Krasnoiarskiy@pfizer.com>
2022-07-17 21:01:34 +0200

    try-catch

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
+import snowflake
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -24,14 +25,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
-
-# using pandas to convert json data into table format
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# shows dataframe using streamlit function
-streamlit.dataframe(fruityvice_normalized)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    # streamlit.write('The user entered ', fruit_choice)
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+        # using pandas to convert json data into table format
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        # shows dataframe using streamlit function
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -40,4 +46,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-fruit_choice2 = streamlit.text_input('What fruit would you like information about?','jackfrut')
+add_fruit = streamlit.text_input('What fruit would you like information about?','from streamlit')
+
+streamlit.write('Thanks for adding', add_fruit)
+
+my_cur.execute(f"insert into fruit_load_list values ('{add_fruit}');")
